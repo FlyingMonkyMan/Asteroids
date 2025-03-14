8cb468c1ad5eee530ba0442e5a121e685442a5f4
FlyingMonkyMan <rossrichards92@gmail.com>
2025-03-14 15:46:18 -0400

    Added player rotation

--- asteroids/player.py
+++ asteroids/player.py
@@ -20,3 +20,14 @@ class Player(CircleShape):
         self.points = self.triangle()
         line_width = 2
         pygame.draw.polygon(screen, self.color, self.points, line_width)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
