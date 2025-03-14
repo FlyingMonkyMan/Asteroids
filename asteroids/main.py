db9b8873519c372ca581b8114f95dea140e4f988
FlyingMonkyMan <rossrichards92@gmail.com>
2025-03-14 15:09:11 -0400

    Added player shape

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,23 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    game_clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), PLAYER_RADIUS)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}"
-          f"\nScreen height: {SCREEN_HEIGHT}")
-    
+        dt = game_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
