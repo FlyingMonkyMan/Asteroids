90aeaf285c998955cc56aa1364a144eb66ceda5b
FlyingMonkyMan <rossrichards92@gmail.com>
2025-03-14 17:59:28 -0400

    Created sprite groups

--- asteroids/main.py
+++ asteroids/main.py
@@ -6,17 +6,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
-    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), PLAYER_RADIUS)
+    
     dt = 0
 
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+    
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), PLAYER_RADIUS)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable_group.update(dt)
+        for object in drawable_group:
+            object.draw(screen)
         pygame.display.flip()
 
         dt = game_clock.tick(60) / 1000
